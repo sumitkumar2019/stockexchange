b97d5f0b6e2682f868b1c617fec5302963b3b7a4
maddy <skumar2019@outlook.com>
2018-07-22 23:37:52 +0200

    Added Heroku Postgres DB Configurations for Production

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -17,7 +17,7 @@ module.exports = {
         }
     },
     production: {
-        client: 'postgresql',
+        client: 'pg',
         connection: {
             database: 'postgres://sivpdezuayldke:527c3588e5caba0b88487a39678cf9ca9dc777cab1d77f901ce3ad3ca65019ad@ec2-174-129-192-200.compute-1.amazonaws.com:5432/dj714rs23jept'
         },
