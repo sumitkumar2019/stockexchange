60a660d64d5c2dfb08a82b4c7fb8d76a3ec9b2d3
maddy <skumar2019@outlook.com>
2018-07-22 22:45:24 +0200

    Added Heroku Postgres DB Configurations

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,10 +4,10 @@ module.exports = {
     development: {
         client: 'postgresql',
         connection: {
-            host : 'localhost',
-            user : 'postgres',
-            password : 'xxxxxx',
-            database : 'xxxxxx'
+            host : 'ec2-54-204-18-53.compute-1.amazonaws.com',
+            user : 'caqdvjfhsznbpp',
+            password : 'f52e92bbb9ca7d7ca08a3b93254a38fd29f666defb302a12a629acc0540705d1',
+            database : 'd1psftfsviir4l'
         },
         migrations:{
             directory:'../migrations'
